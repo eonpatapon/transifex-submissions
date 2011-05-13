bd52bb5203f352cf3ebf84b3e0f13bf278cdf62d
Jean-Philippe Braun <jpbraun@mandriva.com>
2011-05-13 16:31:42 +0200

    option to initialize all projects update doc

--- txsubmissions/management/commands/init_vcs_project.py
+++ txsubmissions/management/commands/init_vcs_project.py
@@ -19,6 +19,8 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('-p', '--project', dest='project',
             help='Initialize a new VCS project'),
+        make_option('-a', '--all', action='store_true', dest='all',
+            help='Initialize all VCS projects'),
         make_option('-l', '--list', action='store_true', dest='list',
             help='List available projects'),
     )
@@ -28,7 +30,13 @@ class Command(BaseCommand):
         if options['list']:
             print "Available projects :"
             for project in VCSProject.objects.all():
-                print "- %s" % project
+                print "- %s" % project.tx_project.slug
+            sys.exit()
+
+        if options['all']:
+            projects = VCSProject.objects.all()
+            for project in projects:
+                self.init_project(project)
             sys.exit()
 
         if options['project']:
@@ -38,8 +46,14 @@ class Command(BaseCommand):
                 print "Unknow project"
                 sys.exit(1)
             else:
-                project.checkout()
-                project.update()
-                project.init_tx_client()
-                project.init_tx_client_resources()
-                print "Done"
+                self.init_project(project)
+            sys.exit()
+
+
+    def init_project(self, project):
+        print "Initialize project %s ..." % project.tx_project.slug
+        project.checkout()
+        project.update()
+        project.init_tx_client()
+        project.init_tx_client_resources()
+        print "Done."
